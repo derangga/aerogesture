d8033bd29f9103f6dd47878aaee141baad9b7bef
Dimas Rangga <dimas.armando@sociolla.com>
2026-07-03 18:34:23 +0700

    chore: bump version to 0.2.0

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
       {
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "aerogesture";
-          version = "0.1.2";
+          version = "0.2.0";
 
           src = ./.;
 
